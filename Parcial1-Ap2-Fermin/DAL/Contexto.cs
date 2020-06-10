b55ba9b90a5b11565b399c01b5af01de9c804f7a
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-10 13:15:34 -0400

    Preparando primer parcial

---
+++ Parcial1-Ap2-Fermin/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Fermin.DAL
+{
+    public class Contexto : DbContext
+    {
+    }
+}
