44d4285dd78d34a2f42e9a933af2396436ae6078
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-10 22:39:53 -0400

    Reubicando la base de datos para el test

---
+++ Parcial1-Ap2-FerminTests/BLL/ArticulosBLLTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1_Ap2_FerminTests.BLL
+{
+    class ArticulosBLLTests
+    {
+    }
+}
---
+++ Parcial1-Ap2-FerminTests1/BLL/ArticulosBLLTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial1_Ap2_Fermin.BLL;
+using Parcial1_Ap2_Fermin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcial1_Ap2_Fermin.BLL.Tests
+{
+    [TestClass()]
+    public class ArticulosBLLTests
+    {
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            bool encontrado = ArticulosBLL.Existe(1);
+
+            Assert.AreEqual(true,encontrado);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetArticulosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
