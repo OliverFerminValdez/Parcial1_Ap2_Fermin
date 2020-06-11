015fb2f94971f859991fc1c7daee050c8dc94fde
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-10 20:23:04 -0400

    Creando modelo

---
+++ Parcial1-Ap2-Fermin/Models/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2_Fermin.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required(ErrorMessage ="Es obligatorio introducir la descripcion")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Es obligatorio introducir la existencia")]
+        public double Existencia { get; set; }
+        [Required(ErrorMessage ="Es obligatorio Introducir el costo")]
+        public double Costo { get; set; }
+        public double ValorInventario { get; set; }
+
+    }
+}
