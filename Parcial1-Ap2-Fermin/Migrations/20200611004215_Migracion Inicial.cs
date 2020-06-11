ce0600a69438267db1544cbd94580670b9b8c468
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-10 20:43:46 -0400

    Añadiendo migracion y creando base de datos

---
+++ Parcial1-Ap2-Fermin/Migrations/20200611004215_Migracion Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_Ap2_Fermin.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Existencia = table.Column<double>(nullable: false),
+                    Costo = table.Column<double>(nullable: false),
+                    ValorInventario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
